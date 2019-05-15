5f724b544a10f149317124ea91408489f7e141a9
AndrewUsamaNassef <46010408+AndrewUsamaNassef@users.noreply.github.com>
2019-05-15 17:05:12 +0200

    Add files via upload

---
+++ src/main/java/com/example/mac/gradproj2/BackgroundWorker3.java
@@ -0,0 +1,89 @@
+package com.example.mac.gradproj2;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class BackgroundWorker3 extends AsyncTask<String , Void , String> {
+    Context context;
+    AlertDialog alertDialog;
+    MapsActivity JSON;
+
+    BackgroundWorker3(Context ctx, MapsActivity json) {
+        context = ctx;
+        JSON = json;
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        String type = params[0];
+        String select_url = "http://192.168.43.23/webapp/select.php";
+        if (type.equals("select")) {
+            try {
+                String id = params[1];
+                URL url = new URL(select_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream OS = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(OS, "UTF-8"));
+                String data = URLEncoder.encode("id", "UTF-8") + "=" + URLEncoder.encode(id, "UTF-8") + "&";
+                bufferedWriter.write(data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                OS.close();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+                String result = "";
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    result += line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return result;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+    @Override
+    protected void onPreExecute() {
+        alertDialog = new AlertDialog.Builder(context).create();
+        alertDialog.setTitle("selecting Status");
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        //alertDialog.setMessage(result);
+        //System.err.println(result);
+        //
+        // alertDialog.show();
+        //if(!result.equals("failed")){
+            JSON.returnJSON(result);
+            System.err.println(JSON);
+        //}
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+    }
+}
