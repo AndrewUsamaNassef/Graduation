87a3a31a1d635beb9686a63ed142e8fa34663204
andrew usama <dodonassef97@gmail.com>
2019-05-15 16:40:02 +0200

    Create exec.php

---
+++ exec.php
@@ -0,0 +1,11 @@
+<?php
+// require_once("junloc.php");
+// $exec = new junloc();
+// $exec->select();
+require_once("register1.php");
+$exec = new user();
+$exec->$username=$_POST['username'];
+$exec->$password=$_POST['password'];
+$exec->$email=$_POST['email'];
+$exec->insert($_POST['username'],$_POST['password'],$_POST['email']);
+?>
